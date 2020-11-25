c3abe1f18e51d1b19cfe7f579db74dd9580efd45
only-user <root@d2e302045702>
2020-11-25 14:24:45 +0000

    remove src folder

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-try {
-    window.$ = window.jQuery = require('jquery');
-    require('bootstrap');
-    
-    //require('more-packages-installed-with-npm-install');
-
-} catch (e) {}
-
-require('./main');
