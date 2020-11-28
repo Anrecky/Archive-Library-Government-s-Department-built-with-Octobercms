69f2a5ceff20a4be5d50275dc261f723eeaece02
only-user <root@d2e302045702>
2020-11-28 08:23:00 +0000

    Init

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+jQuery(function () {
+    setTimeout(() => {
+        $(".preloader__wrapper").hide();
+    }, 1350);
+    var kearsipanService = $(".services__kearsipan");
+    var perpustakaanService = $(".services__perpustakaan");
+
+    // News AJAX
+    var successCount = 1;
+    $('#loadmore').submit(function (event) {
+        event.preventDefault();
+        $(this).request('onLoadPost', {
+            data: {
+                nFetch: successCount
+            },
+            loading: $.oc.stripeLoadIndicator,
+            success: function (data) {
+                console.log(data.news);
+                data.news.forEach(news => {
+                    $(`<div class="col-md-3 my-2"><div class="card" style="width: 100%;"><img class="card-img-top" src="${news.featured_images[0].path}" alt="${news.featured_images[0].description}"><div class="card-body"><h5 class="card-title">${news.title}</h5><p class="card-text">${news.excerpt}</p></div></div></div>`).insertBefore('#loadmore');
+                });
+                successCount++;
+            }
+        });
+    });
+
+
+})
